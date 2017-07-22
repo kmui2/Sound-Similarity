f7cfb91bf73ef111cd201e27ef8831945e2e76a3
Kevin Mui <kmui2@wisc.edu>
2017-07-22 14:05:30 -0500

    adding post endpoints

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ var express = require('express')
 var app = express()
 var path = require("path");
 
+// spawn_python.js
+var util = require("util");
+
+var spawn = require("child_process").spawn;
+var judements = spawn('python',["judements.py"]);
+
 app.set('port', (process.env.PORT || 8000))
 app.use(express.static(__dirname + '/public'))
 
@@ -9,21 +15,19 @@ app.get('/', function (request, response) {
   response.sendFile(path.join(__dirname + '/public/index.html'));
 })
 
+app.post('/sounds', function(req, res) {
+  
+  judements.stdout.on('data',function(chunk){
+      var textChunk = chunk.toString('utf8');// buffer to string
+      util.log(textChunk);
+  });
+  res.send({body: 'Success'});
+})
+
+app.post('/trials', function(req, res) {
+  res.send({body: 'Success'});
+})
+
 app.listen(app.get('port'), function () {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
-
-// spawn_python.js
-var util = require("util");
-
-var spawn = require("child_process").spawn;
-var pyprocess = spawn('python',["python.py"]);
-
-util.log('readingin')
-
-pyprocess.stdout.on('data',function(chunk){
-
-    var textChunk = chunk.toString('utf8');// buffer to string
-
-    util.log(textChunk);
-});
