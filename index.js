c869042e6ca23267538b7a92592ce77a927ef2b0
Kevin Mui <kmui2@wisc.edu>
2017-07-23 16:51:47 -0500

    added http:// to url

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.set('port', (process.env.PORT || 8000))
 app.use(express.static(__dirname + '/public'))
 
 app.listen(app.get('port'), function () {
-  console.log("Node app is running at localhost:" + app.get('port'))
+  console.log("Node app is running at http://localhost:" + app.get('port'))
 })
 
 app.get('/', function (request, response) {
