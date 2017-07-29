14034942efc0896c78f50d5203c1b81e981b92e2
Kevin Mui <kmui2@wisc.edu>
2017-07-29 00:24:50 -0500

    added extra comment to delete

--- public/main.js
+++ public/main.js
@@ -2,7 +2,7 @@
 $(document).ready(function(){
 
     // This listens to the form on-submit action
-    $("form").submit(function(){
+    $("form").submit(function(){    //Remove
 
 
         
@@ -29,7 +29,7 @@ $(document).ready(function(){
                 runExperiment(trials, name, workerId, assignmentId, hitId);
             }
         })
-    });
+    }); // Remove
 
 });
 
