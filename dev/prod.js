a65a4cc0f2c5aa0bb21dcced15d4c8863adb74b8
kmui2 <kmui2@wisc.edu>
2017-10-28 23:53:21 -0500

    bootstrap prod

--- dev/prod.js
+++ dev/prod.js
@@ -12,8 +12,8 @@ $(document).ready(function(){
         let assignmentId = 'assignmentId';
         let hitId = 'hitId';
 
-        $("#loading").html('Loading trials... please wait. </br> <img src="img/preloader.gif">')
-
+        $("#loading").html('<h2 style="text-align:center;">Loading trials... please wait.</h2> </br> <div  class="col-md-2 col-md-offset-5"><img src="img/preloader.gif"></div>')
+        
         // This calls server to run python generate trials (judements.py) script
         // Then passes the generated trials to the experiment
         $.ajax({
